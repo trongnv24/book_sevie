ff18c9e19ec1291174fe361fb4dde67e092d92a4
trongnv24 <viettrong2kk4@gmail.com>
2023-05-30 14:26:50 +0700

    Create new file hello.txt

--- src/main/java/org/training_java/book_rud/service/impl/BookServiceImpl.java
+++ src/main/java/org/training_java/book_rud/service/impl/BookServiceImpl.java
@@ -1,6 +1,6 @@
 package org.training_java.book_rud.service.impl;
 
-import org.springframework.data.domain.Page;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.training_java.book_rud.dto.request.BookRequest;
 import org.training_java.book_rud.dto.response.BookReponse;
@@ -8,24 +8,52 @@ import org.training_java.book_rud.entity.Book;
 import org.training_java.book_rud.repository.BookRepository;
 import org.training_java.book_rud.service.BookService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class BookServiceImpl implements BookService {
+ public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
     @Override
     public BookRepository create(BookRequest request) {
-        return null;
+
+        Book book = Book.from(request);
+        book = bookRepository.save(book);
+        BookReponse response = new BookReponse();
+        response.setName(book.getName());
+        response.setAuthor(book.getAuthor());
+        response.setCode(book.getCode());
+
+        return bookRepository;
+
+
+    }
+
+    @Override
+    public void deleteById(long id) {
+        bookRepository.deleteById(String.valueOf(id));
     }
 
     @Override
     public List<BookReponse> getAll() {
-        return null;
+        List<Book> list = bookRepository.findAll();
+        List<BookReponse> response = new ArrayList<>();
+        for (Book book : list) {
+            BookReponse bookResponse = new BookReponse();
+            bookResponse.setId(book.getId());
+            bookResponse.setName(book.getName());
+            bookResponse.setAuthor(book.getAuthor());
+            bookResponse.setCode(book.getCode());
+
+            response.add(bookResponse);
+        }
+        return response;
     }
 
-    @Override
-    public BookReponse getOneById(String id) {
-        return null;
-    }
 
     @Override
     public BookReponse update(BookRequest request, String id) {
@@ -37,6 +65,25 @@ public class BookServiceImpl implements BookService {
 
     }
 
+
+    @Override
+    public BookReponse getOneById(String Id) {
+
+        Optional<Book> book = bookRepository.findById(getOneById());
+        BookReponse response = new BookReponse();
+        response.setId(book.get().getId());
+        response.setName(book.get().getName());
+        response.setAuthor(book.get().getAuthor());
+        response.setCode(book.get().getCode());
+
+
+        return response;
+
+    }
+
+    private String getOneById() {
+        return null;
+    }
 
 
 }
